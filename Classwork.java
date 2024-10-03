d3cc11b69fbe1f83f1a750c32e1b6285ef29d993
jjennyvc <nyho100>
2024-10-03 10:58:15 -0700

    skibidi toilet

--- Classwork.java
+++ Classwork.java
@@ -2,6 +2,21 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    System.out.println("Hello");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What type of item do you want to buy?");
+    String item;
+    item = sc.nextLine();
+    System.out.println("How many items do you want to buy?");
+    int number;
+    number = sc.nextInt();
+    System.out.println("How much does it weight?");
+    double weight;
+    weight = sc.nextDouble();
+    double totalWeight = number * weight;
+
+    System.out.println(number + " " + item + "at" + weight + "pounds each will weight" + totalWeight + "pounds total");
+    System.out.println("\"That brain of mine is something more than merely mortal; as time will show.\"\nAda Lovelace\nThe first computer programmer");
+    System.out.println("(\\(\\\n( - -)\n(( \' ) ( \' )");
   }
 }
